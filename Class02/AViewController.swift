fb3aee2a514d66029ae43fb22702a998b8f126b7
njcit-09 <2443380740@qq.com>
2019-07-09 09:43:35 +0800

    123

---
+++ Class02/AViewController.swift
@@ -0,0 +1,36 @@
+//
+//  AViewController.swift
+//  Class02
+//
+//  Created by njcit-09 on 2019/7/9.
+//  Copyright © 2019 21. All rights reserved.
+//
+
+import UIKit
+
+class AViewController: UIViewController {
+@IBOutlet weak var lbMsg: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func btnTestClicked(_ sender: UIButton) {
+        print("我按了啥？")
+        lbMsg . text = "我要铜锣烧"
+        lbMsg . textColor = UIColor . red
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Class02/ViewController.swift
+++ Class02/ViewController.swift
@@ -9,18 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
     @IBOutlet weak var lbMsg: UILabel!
     
     var result : float_t = 10.2
     
     @IBAction func btnTestClicked(_ sender: UIButton) {
-        
         print("我按了啥？")
-        
         lbMsg . text = "我要铜锣烧"
         lbMsg . textColor = UIColor . red
     }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
